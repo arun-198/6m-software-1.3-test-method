f193c7113e57e8d01a5f664efa63658ba4b5c528
arun-198 <n.arunalagappan@gmail.com>
2024-06-09 18:03:23 +0800

    Completed assignment 1.3 - passed all tests

--- assignment/q1-control-flow.js
+++ assignment/q1-control-flow.js
@@ -11,5 +11,12 @@ const env = "DEV"; // Toggle between these possible values "DEV" | "STAGE" | "PR
 let databaseCredential = "devuser:password";
 
 // Task: Add code here
+if (env === "DEV") {
+    pass;
+} else if (env === "STAGE") {
+    databaseCredential = "stageuser:password";
+} else if (env === "PROD") {
+    databaseCredential = "produser:password";
+}
 
 console.log(`Database credential for environment ${env} is ${databaseCredential}`);
